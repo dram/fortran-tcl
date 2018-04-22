c8ed977b565574b722f74531a41ddd38f2538335
Xin Wang <dram.wang@gmail.com>
2018-04-22 14:27:59 +0800

    Constants should be declared with parameter attribute.

--- sources/constants.c
+++ sources/constants.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <tcl.h>
 
+#define INT_PARAMETER "integer(c_int), parameter :: "
+
 int main(void)
 {
-	printf("integer(c_int) :: tcl_error = %d\n", TCL_ERROR);
-	printf("integer(c_int) :: tcl_stderr = %d\n", TCL_STDERR);
-	printf("integer(c_int) :: tcl_stdin = %d\n", TCL_STDIN);
-	printf("integer(c_int) :: tcl_stdout = %d\n", TCL_STDOUT);
+	printf(INT_PARAMETER "tcl_error = %d\n", TCL_ERROR);
+	printf(INT_PARAMETER "tcl_stderr = %d\n", TCL_STDERR);
+	printf(INT_PARAMETER "tcl_stdin = %d\n", TCL_STDIN);
+	printf(INT_PARAMETER "tcl_stdout = %d\n", TCL_STDOUT);
 
 	return 0;
 }
